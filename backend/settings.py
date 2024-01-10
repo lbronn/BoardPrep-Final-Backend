f62c6d9151e07dcdb99f5d36eebc449273322ece
tr1b1iwnl <tr1b1iwnl@gmail.com>
2024-01-10 15:12:39 +0800

    Modified cors

--- backend/settings.py
+++ backend/settings.py
@@ -78,7 +78,7 @@ REST_FRAMEWORK = {
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+    "https://boardprep.vercel.app",
     # ... other allowed origins ...
 ]
 
